d606e65ed6e685f78e8b508d565d4af99e6184bf
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-07-28 13:10:57 -0700

    Added query to demonstrate WHERE and ORDER BY

--- EntityFrameworkQueries/Form1.Designer.cs
+++ EntityFrameworkQueries/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnSelectAllVendors = new System.Windows.Forms.Button();
+            this.BtnAllCaliVendors = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnSelectAllVendors
+            // 
+            this.BtnSelectAllVendors.Location = new System.Drawing.Point(62, 59);
+            this.BtnSelectAllVendors.Name = "BtnSelectAllVendors";
+            this.BtnSelectAllVendors.Size = new System.Drawing.Size(383, 58);
+            this.BtnSelectAllVendors.TabIndex = 0;
+            this.BtnSelectAllVendors.Text = "SELECT*FROM Vendors";
+            this.BtnSelectAllVendors.UseVisualStyleBackColor = true;
+            this.BtnSelectAllVendors.Click += new System.EventHandler(this.BtnSelectAllVendors_Click);
+            // 
+            // BtnAllCaliVendors
+            // 
+            this.BtnAllCaliVendors.Location = new System.Drawing.Point(62, 176);
+            this.BtnAllCaliVendors.Name = "BtnAllCaliVendors";
+            this.BtnAllCaliVendors.Size = new System.Drawing.Size(1113, 58);
+            this.BtnAllCaliVendors.TabIndex = 1;
+            this.BtnAllCaliVendors.Text = "SELECT*FROM Vendors WHERE VendorState=\'CA\' ORDER BY VendorName ASC";
+            this.BtnAllCaliVendors.UseVisualStyleBackColor = true;
+            this.BtnAllCaliVendors.Click += new System.EventHandler(this.BtnAllCaliVendors_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1467, 842);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnAllCaliVendors);
+            this.Controls.Add(this.BtnSelectAllVendors);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,5 +77,9 @@
         }
 
         #endregion
+
+        private Button BtnSelectAllVendors;
+        private Button BtnAllCaliVendors;
+        private Button button3;
     }
 }
--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -1,3 +1,6 @@
+using EntityFrameworkQueries.Data;
+using EntityFrameworkQueries.Models;
+
 namespace EntityFrameworkQueries
 {
     public partial class Form1 : Form
@@ -5,6 +8,38 @@ namespace EntityFrameworkQueries
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using APContext dbContext = new APContext();
+
+            // Note to self: Due to changing the namespace of the files
+            // ie. needing to add a using .Data/.Models
+            // The database APContext was missing the same files
+            // So I needed to add a using.models to the context in order to use dbContext properly
+
+            // LINQ (Language Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                       select v).ToList();
+        }
+
+        private void BtnAllCaliVendors_Click(object sender, EventArgs e)
+        {
+            using APContext dbContext = new();
+
+            List<Vendor> vendorList = dbContext.Vendors
+                                        .Where(v => v.VendorState == "CA")
+                                        .OrderBy(v => v.VendorName)
+                                        .ToList();
+
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                       where v.VendorState == "CA"
+                                       orderby v.VendorName
+                                       select v).ToList();
         }
     }
 }
